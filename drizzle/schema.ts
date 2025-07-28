e848ebde2b29fd04824097c9ffecd6f6b99f0067
thezohaibkhalid <zohaibkhalid.pk@gmail.com>
2025-07-28 15:15:55 +0500

    added Logics for video uploading streaming thumbnail uploading to buckets VideoSchema and saving Video Details to our Database

--- drizzle/schema.ts
+++ drizzle/schema.ts
@@ -3,7 +3,9 @@ import {
   text,
   timestamp,
   boolean,
+  uuid,
   integer,
+  jsonb,
 } from "drizzle-orm/pg-core";
 
 export const user = pgTable("user", {
@@ -66,9 +68,27 @@ export const verification = pgTable("verification", {
   ),
 });
 
+export const videos = pgTable("videos", {
+  id: uuid("id").primaryKey().defaultRandom().unique(),
+  title: text("title").notNull(),
+  description: text("description").notNull(),
+  videoUrl: text("video_url").notNull(),
+  videoId: text("video_id").notNull(),
+  thumbnailUrl: text("thumbnail_url").notNull(),
+  visibility: text("visibility").$type<"public" | "private">().notNull(),
+  userId: text("user_id")
+      .notNull()
+      .references(() => user.id, { onDelete: "cascade" }),
+  views: integer("views").notNull().default(0),
+  duration: integer("duration"),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  updatedAt: timestamp("updated_at").notNull().defaultNow(),
+});
+
 export const schema = {
   user,
   session,
   account,
   verification,
+  videos,
 };
---
+++ lib/actions/video.ts
@@ -0,0 +1,66 @@
+"use server"
+import { auth } from "@/lib/auth";
+import { apiFetch, getEnv, withErrorHandling } from "../utils"
+import { headers } from "next/headers";
+import { BUNNY } from "@/constants";
+import {db} from "@/drizzle/db"
+const VIDEO_STREAM_BASE_URL = BUNNY.STREAM_BASE_URL
+const THUMBNAIL_STORAGE_BASE_URL = BUNNY.STORAGE_BASE_URL;
+const THUMBNAIL_CDN_URL = BUNNY.CDN_URL;
+const BUNNY_VIDEO_LIBRARY_ID = getEnv("BUNNY_VIDEO_LIBRARY_ID");
+const ACCESS_KEYS = {
+    streamAccessKey: getEnv("BUNNY_STREAM_ACCESS_KEY"),
+    storageAccessKey: getEnv("BUNNY_STORAGE_ACCESS_KEY"),
+}
+
+//Helper funcitons 
+const getSessionUserId = async (): Promise<string> => {
+    const session = await auth.api.getSession({headers: await headers()});
+    if (!session ) throw new Error("User not authenticated");
+    return session.user.id;
+}
+
+//server functino
+export const getVideoUploadUrl = withErrorHandling(async()=>{
+    await getSessionUserId();
+    const videoResponse = await apiFetch(`${VIDEO_STREAM_BASE_URL}/${BUNNY_VIDEO_LIBRARY_ID}/videos`,{
+        method: "POST",
+        bunnyType:"stream",
+        body:{title:"New Video", collectionId:""},
+    },     )
+    const uploadUrl = `${VIDEO_STREAM_BASE_URL}/${BUNNY_VIDEO_LIBRARY_ID}/videos/${videoResponse.guid}`
+    return {
+        uploadUrl,
+        videoId: videoResponse.guid,
+        accessKey: ACCESS_KEYS.streamAccessKey,
+    };
+}
+)
+
+const getThumbnailUploadUrl = withErrorHandling(async (videoId:string)=>{
+    const fileName = `${Date.now()}-${videoId}-thumbnail`;
+    const uploadUrl = `${THUMBNAIL_STORAGE_BASE_URL}/thumbnails/${fileName}`;
+    const cdnUrl = `${THUMBNAIL_CDN_URL}/thumbnails/${fileName}`;
+
+    return {
+        uploadUrl,
+        cdnUrl,
+        accessKey: ACCESS_KEYS.storageAccessKey,
+    };
+
+})
+
+
+export const saveVideoDetails = withErrorHandling(async(videoDetails:VideoDetails)=>{
+    const userId = await getSessionUserId();
+    await apiFetch(`${VIDEO_STREAM_BASE_URL}/${BUNNY_VIDEO_LIBRARY_ID}/videos/${videoDetails.videoId}`,{
+        method:"POST",
+        bynnyType:"stream",
+        body: {
+            title: videoDetails.title,
+            description: videoDetails.description,
+        }
+    }
+    )
+    await db.insert(videos).values({})
+})
