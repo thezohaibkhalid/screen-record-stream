41b6a6fd7c74b52f58bd6fe66b5a989c167b8053
thezohaibkhalid <zohaibkhalid.pk@gmail.com>
2025-08-01 17:17:34 +0500

    tested video record and restart and upload after recording funtionality added

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,45 +4,45 @@ import {getAllVideos} from "@/lib/actions/video";
 
 import EmptyState from "@/components/EmptyState";
 const Page = async ({searchParams}:SearchParams) => {
-const {query, filter, page} = await searchParams;
-const {videos, pagination} = await getAllVideos(query, filter, Number(page)||1)
+    const {query, filter, page} = await searchParams;
+    const {videos, pagination} = await getAllVideos(query, filter, Number(page)||1)
 
-  return (
-    <main className="wrapper page">
-      <Header title="All Videos" subHeader="Public Library" />
-
-
-        {videos.length>0 ?
-            (
-                <section className="video-grid">
-
-                    {videos.map(({ video, user }) => (
-                          <VideoCard
-                              key={video.id}
-                              id={video.videoId}
-                              title={video.title}
-                              thumbnail={video.thumbnailUrl}
-                              createdAt={video.createdAt}
-                              userImg={user?.image ?? ""}
-                              username={user?.name ?? "Guest"}
-                              views={video.views}
-                              visibility={video.visibility}
-                              duration={video.duration}
-                          />
-                      ))}
+    return (
+        <main className="wrapper page">
+            <Header title="All Videos" subHeader="Public Library" />
 
 
-                </section>
-            ):(
-            <EmptyState
-                icon="/assets/icons/video.svg"
-                title="No Videos Found"
-                description="Try adjusting your search."
-            />)
-        }
+            {videos.length>0 ?
+                (
+                    <section className="video-grid">
 
-    </main>
-  );
+                        {videos.map(({ video, user }) => (
+                            <VideoCard
+                                key={video.id}
+                                id={video.videoId}
+                                title={video.title}
+                                thumbnail={video.thumbnailUrl}
+                                createdAt={video.createdAt}
+                                userImg={user?.image ?? ""}
+                                username={user?.name ?? "Guest"}
+                                views={video.views}
+                                visibility={video.visibility}
+                                duration={video.duration}
+                            />
+                        ))}
+
+
+                    </section>
+                ):(
+                    <EmptyState
+                        icon="/assets/icons/video.svg"
+                        title="No Videos Found"
+                        description="Try adjusting your search."
+                    />)
+            }
+
+        </main>
+    );
 };
 
 export default Page;
--- app/(root)/upload/page.tsx
+++ app/(root)/upload/page.tsx
@@ -41,6 +41,34 @@ const page =  () => {
       setVideoDuration(video.duration);
     }
   }, [video.duration]);
+  useEffect(() => {
+    const checkForRecordedVideo = async()=>{
+      try{
+        const stored = sessionStorage.getItem('recordedVideo');
+        if(!stored) return
+        const {url, name, type, duration} = JSON.parse(stored);
+        const blob = await fetch(url).then((res)=>res.blob())
+        const file = new File([blob], name, {type, lastModified:Date.now()})
+        if(video.inputRef.current) {
+          const dataTransfer = new DataTransfer();
+          dataTransfer.items.add(file);
+          video.inputRef.current.files = dataTransfer.files
+          const event = new Event('change', {bubbles: true})
+          video.inputRef.current?.dispatchEvent(event);
+          video.handleFileChange({
+            target:{files: dataTransfer.files}
+          } as ChangeEvent<HTMLInputElement>)
+        }
+        if(duration) setVideoDuration(duration)
+        sessionStorage.removeItem('recordedVideo')
+        URL.revokeObjectURL(url);
+
+      }catch(err) {
+          console.error(err, "Error occured loading recorded video")
+    }
+    }
+    checkForRecordedVideo();
+  }, [video]);
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
